91700e0a9fe04bd1081e8419f15a261de4dada41
Vite Gourmand <dev@vite-gourmand.fr>
2025-12-30 18:27:17 +0100

    Configuration du point d'entrée React

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,19 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
-import { MenuProvider } from './context/MenuContext';
-import './styles/global.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <MenuProvider>
-          <App />
-        </MenuProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
